210ea886d98e9258c5288e45f6458d9163051830
VitalyProlomov <71337852+VitalyProlomov@users.noreply.github.com>
2021-10-11 23:32:24 +0300

    Add files via upload
    cmake-build-debug folder

---
+++ cmake-build-debug/CMakeFiles/HW1.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/HW1/complex_number.cpp" "D:/HW1/cmake-build-debug/CMakeFiles/HW1.dir/complex_number.cpp.obj"
+  "D:/HW1/container.cpp" "D:/HW1/cmake-build-debug/CMakeFiles/HW1.dir/container.cpp.obj"
+  "D:/HW1/main.cpp" "D:/HW1/cmake-build-debug/CMakeFiles/HW1.dir/main.cpp.obj"
+  "D:/HW1/number.cpp" "D:/HW1/cmake-build-debug/CMakeFiles/HW1.dir/number.cpp.obj"
+  "D:/HW1/polar_coordinates.cpp" "D:/HW1/cmake-build-debug/CMakeFiles/HW1.dir/polar_coordinates.cpp.obj"
+  "D:/HW1/rational_number.cpp" "D:/HW1/cmake-build-debug/CMakeFiles/HW1.dir/rational_number.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/HW1.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW1.dir/complex_number.cpp.obj"
+  "CMakeFiles/HW1.dir/container.cpp.obj"
+  "CMakeFiles/HW1.dir/main.cpp.obj"
+  "CMakeFiles/HW1.dir/number.cpp.obj"
+  "CMakeFiles/HW1.dir/polar_coordinates.cpp.obj"
+  "CMakeFiles/HW1.dir/rational_number.cpp.obj"
+  "HW1.exe"
+  "HW1.exe.manifest"
+  "HW1.pdb"
+  "libHW1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
